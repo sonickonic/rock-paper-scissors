4508d747e011a7f2d99604b3fb77e145dddddf9e
Sofia Fateyev <pateyev.sofia@gmail.com>
2020-09-17 11:31:10 +0100

    Extract Chip component into it own file

---
+++ src/Chip.js
@@ -0,0 +1,49 @@
+import React from "react";
+import styled from "styled-components";
+
+const ChipContainer = styled.div`
+  position: relative;
+  background: linear-gradient(
+    ${(props) => props.lightColor},
+    ${(props) => props.darkColor}
+  );
+  width: 13.4rem;
+  height: 13.4rem;
+  border-radius: 100%;
+  box-shadow: inset 0px -6px 0px rgba(0, 0, 0, 0.25);
+  cursor: pointer;
+
+  :nth-child(3) {
+    margin: 1.5rem auto 0;
+  }
+
+  ::before {
+    content: "";
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 10rem;
+    height: 10rem;
+    border-radius: 100%;
+    background-color: var(--white);
+    box-shadow: inset 0px 4px 0px rgba(0, 0, 0, 0.25);
+  }
+`;
+
+const Shape = styled.img`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+function Chip({ shape }) {
+  return (
+    <ChipContainer lightColor={shape.lightColor} darkColor={shape.darkColor}>
+      <Shape src={shape.imageSrc} alt={shape.name} />
+    </ChipContainer>
+  );
+}
+
+export default Chip;
--- src/InitialGame.js
+++ src/InitialGame.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 import shapes from "./data";
+import Chip from "./Chip";
 import Background from "./images/bg-triangle.svg";
 
 const Container = styled.div`
@@ -15,53 +16,11 @@ const Container = styled.div`
   justify-content: space-between;
 `;
 
-const Chip = styled.div`
-  position: relative;
-  background: linear-gradient(
-    ${(props) => props.lightColor},
-    ${(props) => props.darkColor}
-  );
-  width: 13.4rem;
-  height: 13.4rem;
-  border-radius: 100%;
-  box-shadow: inset 0px -6px 0px rgba(0, 0, 0, 0.25);
-
-  :last-child {
-    margin: 1.5rem auto 0;
-  }
-
-  ::before {
-    content: "";
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    width: 10rem;
-    height: 10rem;
-    border-radius: 100%;
-    background-color: var(--white);
-    box-shadow: inset 0px 4px 0px rgba(0, 0, 0, 0.25);
-  }
-`;
-
-const Shape = styled.img`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-`;
-
 function InitialGame() {
   return (
     <Container>
       {shapes.map((shape) => (
-        <Chip
-          key={shape.name}
-          lightColor={shape.lightColor}
-          darkColor={shape.darkColor}
-        >
-          <Shape src={shape.imageSrc} alt={shape.name} />
-        </Chip>
+        <Chip shape={shape} key={shape.name} />
       ))}
     </Container>
   );
