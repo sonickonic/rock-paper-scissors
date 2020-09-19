5971811e9de34267bd3cda1605ccfd2ec6d619ac
Sofia Fateyev <pateyev.sofia@gmail.com>
2020-09-19 17:23:40 +0100

    Add shapes based on the extend game

--- src/data.js
+++ src/data.js
@@ -1,6 +1,8 @@
 import rock from "./images/icon-rock.svg";
 import scissors from "./images/icon-scissors.svg";
 import paper from "./images/icon-paper.svg";
+import lizard from "./images/icon-lizard.svg";
+import spock from "./images/icon-spock.svg";
 
 const shapes = [
   {
@@ -8,21 +10,35 @@ const shapes = [
     imageSrc: paper,
     lightColor: "var(--paper)",
     darkColor: "var(--paper-darker)",
-    beats: "rock",
+    beats: ["rock", "spock"],
   },
   {
     name: "scissors",
     imageSrc: scissors,
     lightColor: "var(--scissors)",
     darkColor: "var(--scissors-darker)",
-    beats: "paper",
+    beats: ["paper", "lizared"],
   },
   {
     name: "rock",
     imageSrc: rock,
     lightColor: "var(--rock)",
     darkColor: "var(--rock-darker)",
-    beats: "scissors",
+    beats: ["scissors", "lizared"],
+  },
+  {
+    name: "lizard",
+    imageSrc: lizard,
+    lightColor: "var(--lizard)",
+    darkColor: "var(--lizard-darker)",
+    beats: ["spock", "paper"],
+  },
+  {
+    name: "spock",
+    imageSrc: spock,
+    lightColor: "var(--cyan)",
+    darkColor: "var(--cyan-darker)",
+    beats: ["rock", "scissors"],
   },
 ];
 
