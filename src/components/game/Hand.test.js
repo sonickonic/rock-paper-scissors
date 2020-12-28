28b75279b60537a22e8f1515738d9757fe3773cd
Sofia Fateyev <pateyev.sofia@gmail.com>
2020-12-28 19:04:30 +0000

    Add tests for hand component

---
+++ src/components/game/Hand.test.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Hand from "./Hand";
+import shapes from "../../data";
+
+describe("Hand", () => {
+  it("renders correctly", () => {
+    const shape = shapes[0];
+    const { asFragment } = render(<Hand shape={shape} />);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it("shows image", () => {
+    const shape = shapes[0];
+    const { queryByAltText } = render(<Hand shape={shape} />);
+
+    expect(queryByAltText(`${shape.name}`)).toBeTruthy();
+  });
+});
