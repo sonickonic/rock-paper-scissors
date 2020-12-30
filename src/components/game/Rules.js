fffd9c71832b2b134613c3ceed240019d4a323c7
Sofia Fateyev <pateyev.sofia@gmail.com>
2020-12-30 15:16:20 +0000

    Make rules component responsive

--- src/components/game/Rules.js
+++ src/components/game/Rules.js
@@ -16,24 +16,50 @@ const Container = styled.div`
   width: 100%;
   top: 0;
   right: 0;
+
+  @media (min-width: 500px) {
+    padding: 3rem 5rem 3rem;
+    height: 41.5rem;
+    width: 40rem;
+    border-radius: 1rem;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    box-shadow: 0px 0px 0px 100vh rgba(0, 0, 0, 0.7);
+  }
 `;
 
 const CloseButton = styled.a`
   display: inline-block;
   cursor: pointer;
+
+  @media (min-width: 500px) {
+    position: absolute;
+    top: 4rem;
+    right: 5rem;
+  }
 `;
 
 const Title = styled.h3`
   color: var(--text);
   font-size: 3.2rem;
   font-weight: 700;
+
+  @media (min-width: 500px) {
+    align-self: flex-start;
+  }
+`;
+
+const Image = styled.img`
+  width: 30rem;
 `;
 
 const Rules = ({ gameType, handleClick }) => {
   return (
     <Container>
       <Title>rules</Title>
-      <img alt="Game rules" src={gameType.rules} />
+      <Image alt="Game rules" src={gameType.rules} />
       <CloseButton>
         <img onClick={handleClick} alt="Close button" src={IconSrc} />
       </CloseButton>
