9a34c93609e58e435df3c1173d861a3c6d843fb5
Sofia Fateyev <pateyev.sofia@gmail.com>
2020-12-30 19:43:10 +0000

    Make throw component responsive

--- src/components/game/Throw.js
+++ src/components/game/Throw.js
@@ -8,32 +8,51 @@ const Text = styled.p`
   text-align: center;
   color: var(--white);
   margin-top: 2.5rem;
+  letter-spacing: 0.16em;
+
+  @media (min-width: 800px) {
+    font-size: 2rem;
+    margin-bottom: 6rem;
+  }
 `;
 
 const Container = styled.div`
   position: relative;
-  width: 31.5rem;
-  height: 28.3rem;
+  width: 100%;
+  max-width: 47rem;
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
+
+  @media (min-width: 800px) {
+    max-width: 60rem;
+  }
+`;
+
+const HandContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  @media (min-width: 800px) {
+    flex-direction: column-reverse;
+  }
 `;
 
 const Throw = ({ userSelectedHand, botSelectedHand, winner }) => {
   return (
     <Container>
-      <div>
+      <HandContainer>
         <Hand winner={winner} shape={userSelectedHand} />
         <Text>you picked</Text>
-      </div>
-      <div>
+      </HandContainer>
+      <HandContainer>
         {botSelectedHand ? (
           <Hand winner={winner} shape={botSelectedHand} />
         ) : (
           <Placeholder />
         )}
         <Text>the house picked</Text>
-      </div>
+      </HandContainer>
     </Container>
   );
 };
