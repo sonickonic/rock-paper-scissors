d005ca7743d3ecf5bcc42f881d52ae5be9d5d77b
Sofia Fateyev <pateyev.sofia@gmail.com>
2021-01-14 21:39:25 +0000

    Remove unnecessary app component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./components/App";
+import Game from "./components/game";
 import * as serviceWorker from "./serviceWorker";
 import { Provider } from "react-redux";
 import store from "./store";
@@ -9,7 +9,7 @@ import store from "./store";
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Game />
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
